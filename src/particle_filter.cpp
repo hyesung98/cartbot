5f2d305516415a7b374e8bcf85031d93e6e30034
hyesung98 <hs981002@naver.com>
2022-10-13 14:54:54 +0900

    update particle filter

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -2,116 +2,72 @@
 
 PARTICLE_FILTER::PARTICLE_FILTER(int num)
 {
-    numofParticles = num;
-    ptlist.clear();
+    numparticles_ = num;
+    ptlist_.clear();
+    weight_sum_ = 0;
 }
 
-void PARTICLE_FILTER::initCoordination(float x, float y)
+void PARTICLE_FILTER::initCoordination(float init_x, float init_y, ros::Time init_time)
 {
-    bound_x = x;
-    bound_y = y;
-    lost_cnt = 0;
+    ptlist_.clear();
+    particle_cloud_.clear();
+    bf_time_ = init_time;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dis(TRACKING_BOUDARY * -100, TRACKING_BOUDARY * 100);
-    for (int i = 0; i < numofParticles; i++)
+    for (int i = 0; i < numparticles_; i++)
     {
         particle pt;
         pcl::PointXYZ pcl_pt;
-        pt.x = bound_x + dis(gen) * 0.01;
-        pt.y = bound_y + dis(gen) * 0.01;
-        pt.weight = 0;
+        pt.x = init_x + dis(gen) * 0.01;
+        pt.y = init_y + dis(gen) * 0.01;
         pt.weight = 0;
         pcl_pt.x = pt.x;
         pcl_pt.y = pt.y;
         pcl_pt.z = 0;
-        ptlist.push_back(pt);
-        particle_cloud.push_back(pcl_pt);
+        ptlist_.push_back(pt);
+        particle_cloud_.push_back(pcl_pt);
     }
 }
 
-void PARTICLE_FILTER::doPrediction()
+void PARTICLE_FILTER::run(const Model &prediction, const std::vector<Model> &measure)
 {
-    for (int i = 0; i < numofParticles; i++) // human model
-    {
-        particle_cloud.at(i).x = ptlist.at(i).x = ptlist.at(i).x + static_cast<float>(getGaussianRandom(0, 0.1)); // just using Gassian Random Variable
-        particle_cloud.at(i).y = ptlist.at(i).y = ptlist.at(i).y + static_cast<float>(getGaussianRandom(0, 0.1));
-        ptlist.at(i).weight = 0;
-    }
+    doPrediction(prediction);
+    doUpdate(measure);
+    doResampling();
 }
 
-CLUSTER_STATE PARTICLE_FILTER::doUpdate(const cartbot::clusterarray &clusterlist) // Updating Measure Model
+void PARTICLE_FILTER::doPrediction(const Model &model)
 {
-    if(lost_cnt > 50)
-        return CLUSTER_STATE::LOST;
-
-    // Get clusters which are located near boudary
-    std::vector<Point> random_ptlist;
-    std::vector<Point> target_ptlist;
-    float m_avg, m_sum = 0;
-    for (auto cluster : clusterlist.clusters)
+    ros::Time now_time = ros::Time::now();
+    ros::Duration delta_t = now_time - bf_time_;
+    for (int i = 0; i < numparticles_; i++) // human model
     {
-        if (cluster.points.size() > 100) // Too many points
-            continue;
-        if (isNear(cluster))
-        {
-            Point target_pt;
-            target_pt.x = cluster.mid_x;
-            target_pt.y = cluster.mid_y;
-            target_ptlist.push_back(target_pt);
-            for (auto point : cluster.points)
-            {
-                Point random_pt;
-                random_pt.x = point.x;
-                random_pt.y = point.y;
-                random_ptlist.push_back(random_pt);
-            }
-        }
+        /* Constant Speed model*/
+        ptlist_.at(i).x += model.vx * delta_t.toNSec() + static_cast<float>(getGaussianRandom(0, 0.05));
+        ptlist_.at(i).y += model.vy * delta_t.toNSec() + static_cast<float>(getGaussianRandom(0, 0.05));
+        /* Acceleration Speed model */
+        // ptlist_.at(i).x += model.vx * delta_t.toNSec() + (0.5 * model.ax * pow(delta_t.toNSec(), 2)) + static_cast<float>(getGaussianRandom(0, 0.05));
+        // ptlist_.at(i).y += model.vy * delta_t.toNSec() + (0.5 * model.ay * pow(delta_t.toNSec(), 2)) + static_cast<float>(getGaussianRandom(0, 0.05));
+        particle_cloud_.at(i).x = ptlist_.at(i).x; // just using Gassian Random Variable
+        particle_cloud_.at(i).y = ptlist_.at(i).y;
+        ptlist_.at(i).weight = 0;
     }
-    /* Make Measurement Model */
-    if (target_ptlist.size() > 0)
-    {
-        float avg_target_x = 0, avg_target_y = 0;
-        for (auto pt : target_ptlist)
-        {
-            avg_target_x += pt.x;
-            avg_target_y += pt.y;
-        }
-        bound_x = avg_target_x = avg_target_x / target_ptlist.size();
-        bound_y = avg_target_y = avg_target_y / target_ptlist.size();
-        if (random_ptlist.size() > 12)
-            random_ptlist.erase(random_ptlist.begin() + 6, random_ptlist.end() - 6);
+    bf_time_ = now_time;
+}
 
-        /* mdist --> Target, pdist --> Proposal */
-        for (auto pt : random_ptlist)
-            m_sum += sqrt(pow(pt.x - avg_target_x, 2) + pow(pt.y - avg_target_y, 2));
-        m_avg = m_sum / random_ptlist.size();
-
-        /* Calculate weight of particle */
-        weight_sum = 0;
-        for (auto ptc : ptlist)
-        {
-            float ratio, p_avg, p_sum = 0;
-            for (auto pt : random_ptlist)
-                p_sum += sqrt(pow(ptc.x - avg_target_x, 2) + pow(ptc.y - avg_target_y, 2));
-            p_avg = p_sum / random_ptlist.size();
-            ratio = p_avg / m_avg;
-            if (ratio <= 1.2 && ratio >= 0.8)
-            {
-                ptc.weight = 1 - (abs(p_sum - m_sum) / m_sum);
-            }
-            else
-            {
-                ptc.weight = 0;
-            }
-            weight_sum += ptc.weight;
-        }
-        return CLUSTER_STATE::TRACKING;
-    }
-    else
+void PARTICLE_FILTER::doUpdate(const std::vector<Model> &measurelist) // Updating Measure Model
+{
+    weight_sum_ = 0;
+    for(int i = 0 ; i < numparticles_ ; i++)
     {
-        lost_cnt++;
-        return CLUSTER_STATE::TRACKING;
+        float tmp_weight = 0;
+        for(auto measure : measurelist)
+        {
+            tmp_weight += 1 / sqrt(pow(ptlist_.at(i).x - measure.x, 2) + pow(ptlist_.at(i).y - measure.y , 2));
+        }
+        ptlist_.at(i).weight = tmp_weight;
+        weight_sum_ += tmp_weight;
     }
 }
 
@@ -120,34 +76,30 @@ void PARTICLE_FILTER::doResampling()
     /* Stochastic Universal Resampling */
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<float> dis(0, weight_sum / numofParticles);
+    std::uniform_real_distribution<float> dis(0, weight_sum_ / numparticles_);
     std::vector<particle> resampled_list;
     float r = dis(gen);
-    float c = ptlist.at(0).weight;
+    float c = ptlist_.at(0).weight;
     int i = 0;
-    for(int m = 0 ; m < numofParticles ; m++)
+    for (int m = 0; m < ptlist_.size(); m++)
     {
-        float u = r + (m - 1) * (weight_sum / numofParticles);
-        while(u > c)
+        float u = r + m * (weight_sum_ / numparticles_);
+        while (u > c)
         {
             i = i + 1;
-            std::cout << i << std::endl;
-            c = c + ptlist.at(i).weight;
+            c += ptlist_.at(i).weight;
         }
-        resampled_list.push_back(ptlist.at(i));
+        resampled_list.push_back(ptlist_.at(i));
     }
-    ptlist.clear();
-    ptlist = resampled_list;
-}
-
-bool PARTICLE_FILTER::isNear(cartbot::cluster &cluster)
-{
-    if (cluster.mid_x <= bound_x + TRACKING_BOUDARY && cluster.mid_x >= bound_x - TRACKING_BOUDARY)
+    ptlist_.clear();
+    ptlist_.assign(resampled_list.begin(), resampled_list.end());
+    x_ = 0;
+    y_ = 0;
+    for(auto pt : ptlist_)
     {
-        if (cluster.mid_y <= bound_y + TRACKING_BOUDARY && cluster.mid_y >= bound_y - TRACKING_BOUDARY)
-        {
-            return true;
-        }
+        x_ += pt.x;
+        y_ += pt.y;
     }
-    return false;
+    x_ /= numparticles_;
+    y_ /= numparticles_;
 }
--- src/tracking_node.cpp
+++ src/tracking_node.cpp
@@ -1,12 +1,18 @@
 #include <cartbot/utility.h>
 #include <cartbot/particle_filter.h>
 
-#define DIST_THRESHOLD 0.4
+#define DIST_THRESHOLD 0.5
+#define MIN_DIST 0.6
 ros::Publisher particle_pub;
 ros::Publisher boundary_pub;
+std::vector<Model> measure_buffer;
+std::vector<ros::Time> time_buffer;
+
+std::size_t state_cnt = 0;
+int lost_cnt = 0;
+PARTICLE_FILTER pf(3000);
 
 CLUSTER_STATE state = CLUSTER_STATE::LOST;
-PARTICLE_FILTER pf(3000);
 
 void pusblishBoundary(float bound_x, float bound_y) // Observation Boudary Box
 {
@@ -14,8 +20,8 @@ void pusblishBoundary(float bound_x, float bound_y) // Observation Boudary Box
     box.header.frame_id = "laser_frame";
     box.header.stamp = ros::Time::now();
     box.label = 10;
-    box.dimensions.x = 1;
-    box.dimensions.y = 1;
+    box.dimensions.x = 0.5;
+    box.dimensions.y = 0.5;
     box.dimensions.z = 0.01;
     box.pose.position.x = bound_x;
     box.pose.position.y = bound_y;
@@ -29,36 +35,119 @@ void clusterCallback(const cartbot::clusterarray::ConstPtr &clusterlist)
     switch (state)
     {
     case LOST:
+    {
+        ROS_INFO("STATE----> LOST");
+        measure_buffer.clear();
         for (auto cluster : clusterlist->clusters)
         {
-            float distance = sqrt(pow(cluster.mid_x, 2) + pow(cluster.mid_y, 2));
-            if (distance < DIST_THRESHOLD)
+            float dist = sqrt(pow(cluster.mid_x, 2) + pow(cluster.mid_y, 2));
+            if (dist < DIST_THRESHOLD)
             {
-                pf.initCoordination(cluster.mid_x, cluster.mid_y);
-                particle_pub.publish(cloud2cloudmsg(pf.particle_cloud));
-                pusblishBoundary(pf.getBoundX(), pf.getBoundY());
+                Model tmp_measure;
+                tmp_measure.x = cluster.mid_x;
+                tmp_measure.y = cluster.mid_y;
+                tmp_measure.now_time = cluster.Header.stamp;
+                measure_buffer.push_back(tmp_measure);
+                pusblishBoundary(tmp_measure.x, tmp_measure.y);
                 state = STAGE1;
+                break;
             }
         }
         break;
+    }
+
     case STAGE1:
-        state = TRACKING;
-        break;
-    case STAGE2:
-        break;
-    case STAGE3:
-        break;
-    case TRACKING:
-        pf.doPrediction();
-        pf.doUpdate(*clusterlist);
-        pusblishBoundary(pf.getBoundX(), pf.getBoundY());
-        particle_pub.publish(cloud2cloudmsg(pf.particle_cloud));
-        pf.doResampling();
-        particle_pub.publish(cloud2cloudmsg(pf.particle_cloud));
+    {
+        ROS_INFO("STATE----> STAGE1");
+        Model &last_model = measure_buffer[measure_buffer.size() - 1];
+        bool islost = false;
+        for (auto cluster : clusterlist->clusters)
+        {
+            float dist = sqrt(pow(cluster.mid_x - last_model.x, 2) + pow(cluster.mid_y - last_model.y, 2));
+            if (dist < MIN_DIST)
+            {
+                Model tmp_measure;
+                tmp_measure.x = cluster.mid_x;
+                tmp_measure.y = cluster.mid_y;
+                tmp_measure.now_time = cluster.Header.stamp;
+                ros::Duration delta_t = tmp_measure.now_time - last_model.now_time;
+                if (state_cnt >= 0)
+                {
+                    tmp_measure.vx = (tmp_measure.x - last_model.x) / delta_t.toNSec();
+                    tmp_measure.vy = (tmp_measure.y - last_model.y) / delta_t.toNSec();
+                    if (state_cnt >= 1)
+                    {
+                        tmp_measure.ax = (tmp_measure.vx - last_model.vx) / delta_t.toNSec();
+                        tmp_measure.ay = (tmp_measure.vy - last_model.vy) / delta_t.toNSec();
+                        pf.initCoordination(tmp_measure.x, tmp_measure.y, ros::Time::now());
+                        particle_pub.publish(cloud2cloudmsg(pf.getParticle()));
+                        state = TRACKING;
+                    }
+                    measure_buffer.push_back(tmp_measure);
+                    pusblishBoundary(tmp_measure.x, tmp_measure.y);
+                    islost = true;
+                }
+                state_cnt++;
+                break;
+            }
+        }
+        if (!islost)
+        {
+            state_cnt = 0;
+            state = LOST;
+        }
         break;
     }
+    case TRACKING:
+    {
+        ROS_INFO("STATE----> Tracking");
+        Model &last_model = measure_buffer[measure_buffer.size() - 1];
+        std::vector<Model> particle_model;
+        bool islost = false;
+        for (auto cluster : clusterlist->clusters)
+        {
+            float dist = sqrt(pow(cluster.mid_x - last_model.x, 2) + pow(cluster.mid_y - last_model.y, 2));
+            if (dist < MIN_DIST)
+            {
+                Model tmp_model;
+                tmp_model.x = cluster.mid_x;
+                tmp_model.y = cluster.mid_y;
+                particle_model.push_back(tmp_model);
+                islost = true;
+            }
+        }
+        if (islost)
+        {
+            lost_cnt = 0;
+            pf.run(measure_buffer.at(measure_buffer.size() - 1), particle_model);
+            particle_pub.publish(cloud2cloudmsg(pf.getParticle()));
+            pusblishBoundary(pf.getX(), pf.getY());
+            Model tmp_model;
+            tmp_model.x = pf.getX();
+            tmp_model.y = pf.getY();
+            tmp_model.now_time = clusterlist->clusters.at(0).Header.stamp;
+            ros::Duration delta_t = tmp_model.now_time - last_model.now_time;
+            tmp_model.vx = (tmp_model.x - last_model.x) / delta_t.toNSec();
+            tmp_model.vy = (tmp_model.y - last_model.y) / delta_t.toNSec();
+            tmp_model.ax = (tmp_model.vx - last_model.vx) / delta_t.toNSec();
+            tmp_model.ay = (tmp_model.vy - last_model.vy) / delta_t.toNSec();
+            measure_buffer.push_back(tmp_model);
+            if (measure_buffer.size() > 3)
+                measure_buffer.erase(measure_buffer.begin());
+        }
+        else
+        {
+            lost_cnt++;
+            if(lost_cnt > 5)
+            {
+                state = LOST;
+                lost_cnt = 0;
+            }
+        }
+        break;
+    }
+    }
 }
-
 
 int main(int argc, char **argv)
 {
